72c26279e2776ecbb2de15d8b672e2ffee1ae587
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-04-14 11:47:43 -0400

    Create the `override` decorator to check overriding methods.

---
+++ override.py
@@ -0,0 +1,38 @@
+"""
+A helper decorator to override methods from superclasses.
+"""
+
+
+def override(superclass: type):
+    """
+    Check that a subclass can override a superclass method.
+    """
+
+    assert isinstance(superclass, type), (
+        "The `superclass` attribute must be"
+        " a class with a method which the given method overrides."
+    )
+
+    def check_override(method):
+        """
+        Check that the superclass has a method of the same name.
+        """
+
+        if not hasattr(superclass, method.__name__):
+            raise NotImplementedError(
+                f"The superclass `{superclass}`"
+                f" does not implement the method `{method.__name__}`."
+            )
+
+        superclass_method = getattr(superclass, method.__name__)
+
+        if type(superclass_method) is not type(method):
+            raise NotImplementedError(
+                f"The superclass `{superclass}`"
+                f" has an attribute named `{method.__name__}`"
+                f" that must be a method to override."
+            )
+
+        return method
+
+    return check_override
