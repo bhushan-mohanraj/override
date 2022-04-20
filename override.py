35daead475aee8f147deef201eecfc659d2190da
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-04-20 00:22:05 -0400

    Use the superclass name in error messages.

--- override.py
+++ override.py
@@ -19,14 +19,14 @@ def override(superclass: type):
         """
 
         assert hasattr(superclass, method.__name__), (
-            f"The superclass `{superclass}`"
+            f"The superclass `{superclass.__name__}`"
             f" has no method named `{method.__name__}`."
         )
 
         superclass_method = getattr(superclass, method.__name__)
 
         assert type(superclass_method) is type(method), (
-            f"The superclass `{superclass}`"
+            f"The superclass `{superclass.__name__}`"
             f" has an attribute named `{method.__name__}`,"
             f" but the attribute should be a method to override."
         )
